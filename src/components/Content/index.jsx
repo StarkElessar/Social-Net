251f361af485e7783acca82c2777c87864e870ba
Stark <serj.elessar@gmail.com>
2021-09-08 15:15:23 +0300

    add profile and messages file

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import MyPosts from './../MyPosts';
 import Dialogs from './../Messages';
 import { Route } from 'react-router';
 
 const Content = (props) => {
   return (
-    <Wrapper>
-      <Route path='/' render={ () => <MyPosts/> } />
-      <Route path='/profile' render={ () => <MyPosts/> } />
-      <Route path='/dialogs' render={ () => <Dialogs/> } />
-    </Wrapper>
+      <Wrapper>
+        <Route path='' render={() => <MyPosts />} />
+        <Route path='/profile' render={() => <MyPosts />} />
+        <Route path='/dialogs' render={() => <Dialogs />} />
+      </Wrapper>
+    
   )
 };
 
