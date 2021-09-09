0098a6af7b68281fcee4498b5de83794aacaefe4
Stark <serj.elessar@gmail.com>
2021-09-09 23:11:40 +0300

    add Block for Post

--- src/components/MyPosts/index.jsx
+++ src/components/MyPosts/index.jsx
@@ -6,47 +6,144 @@ import FontAwesomeIcon from "./small-components/FontAwesomeIcon";
 const MyPosts = (props) => {
   return (
     <Wrapper>
-      <AddPost>
-        <Avatar>
-          <img src="https://themified.com/friend-finder/images/users/user-1.jpg" alt="user-avatar" />
-        </Avatar>
-        <form name="add_post" action="#">
-          <textarea id="userPost" name="user_post" rows="2" cols="30" placeholder="Напишите, что вы думаете"></textarea>
-          <FontAwesomeIcon nameClass="far fa-edit" />
-          <FontAwesomeIcon nameClass="far fa-images" />
-          <FontAwesomeIcon nameClass="fas fa-video" />
-          <FontAwesomeIcon nameClass="fas fa-map-marked-alt" />
-          <button>Опубликовать</button>
-        </form>
-      </AddPost>
-      <AllPosts>
-        <h2>Это тестовый текст</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quae itaque neque? Amet dicta officia accusamus modi distinctio dolores aperiam ratione perspiciatis, facilis eaque vero reprehenderit beatae labore architecto culpa esse voluptatum. Totam officiis minus consequatur alias reiciendis nulla consectetur perferendis doloremque nisi quae, tempora eveniet sit nobis, recusandae odio nemo modi, esse harum eaque iste optio! Repudiandae veritatis iure perferendis soluta placeat rerum eveniet ullam odit atque voluptatem laborum numquam assumenda id quam possimus, perspiciatis dolorem vel quis voluptate odio explicabo quia corrupti sequi? Distinctio, quaerat. Voluptatem ipsa molestias molestiae eos nulla a, doloremque totam fugit quam, laudantium explicabo. Possimus dolore aperiam totam adipisci quisquam pariatur, numquam cum amet aliquam similique consectetur odio maiores dolores quasi a blanditiis ipsa. Molestiae totam suscipit necessitatibus odio vel corporis ut, nobis, ad placeat numquam a accusamus ipsum vitae cum quidem delectus ducimus quis soluta. Molestias enim modi nulla deserunt molestiae error quo dolorem neque ducimus voluptate alias nemo, consequatur exercitationem doloremque aliquam officiis obcaecati, ex delectus voluptates atque, quas provident beatae esse! Autem aliquid officiis, harum laboriosam doloremque quaerat est ex voluptas voluptatibus amet beatae sint explicabo magni inventore recusandae magnam incidunt eum mollitia, ipsum doloribus illo molestiae? Saepe, repudiandae, nesciunt beatae praesentium unde voluptates libero similique inventore minima ad commodi doloremque sequi eligendi blanditiis voluptas, debitis et modi omnis fuga obcaecati possimus facilis? Itaque corporis reiciendis sapiente in! Placeat magnam quisquam culpa facilis, saepe dignissimos excepturi eveniet, totam architecto tenetur repudiandae impedit maxime! Aliquid, distinctio sapiente! Repellat fuga perferendis assumenda ducimus asperiores, facilis error ea optio sapiente dicta voluptatum dolores amet reiciendis natus expedita eligendi consequuntur minima eius. Quos animi ea voluptatum nisi quo accusamus voluptatem, suscipit, illum qui, non veritatis deleniti nam inventore. Saepe, fugiat officiis laborum quibusdam quam cumque placeat, ducimus veniam voluptatem aliquam repellat in earum esse eligendi?
-        </p>
-      </AllPosts>
+      <AddPostComponent />
+      <MyPostComponent
+        userAvatarUrl='https://themified.com/friend-finder/images/users/user-1.jpg'
+        authorName='Сарра Круиз'
+        postDate='2 октября в 11:10'
+        userTextPost='Тестим камеру нового Xiaomi redmi Note 3 Pro ✌📷
+                      Скажу одно, соотношение качества всех внутренностей, мощности процессора, камеры и автономности по отношению к бюджету💰 самого аппарата - нереальны! Но даже и эти сравнения не сопоставимы с красотою Вички 😌'
+      />
+      <MyPostComponent
+        userAvatarUrl='https://themified.com/friend-finder/images/users/user-1.jpg'
+        authorName='Сарра Круиз'
+        postDate='3 октября в 13:30'
+        userTextPost='Именно поэтому мы и работаем в SketchUp.
+                      Смотрите запись мастер-класса по моделированию мягкого кресла и сложного светильника.
+                      ⚠Предупреждаем! Возможна реакцкия " А что так можно было?!"⚠
+                      SketchUp - это просто и быстро. Возможности не ограничены, это лишь только кажется. Проще, удобнее, логичнее. Крутой конечный результат.
+                      Смотрите, удивляйтесь, наслаждайтесь!'
+      />
+      <MyPostComponent
+        userAvatarUrl='https://themified.com/friend-finder/images/users/user-1.jpg'
+        authorName='Сарра Круиз'
+        postDate='12 октября в 10:30'
+        userTextPost='На днях пересмотрел Хоббит и Властелин Колец, и как бы хотелось продолжения этой выдуманной вселенной 😥'
+      />
+      
     </Wrapper>
     
   )
 };
 
+const AddPostComponent = (props) => {
+  return (
+    <AddPost>
+      <Avatar>
+        <img src="https://themified.com/friend-finder/images/users/user-1.jpg" alt="user-avatar" />
+      </Avatar>
+      <form name="add_post" action="#">
+        <textarea id="userPost" name="user_post" rows="2" cols="30" placeholder="Напишите, что вы думаете"></textarea>
+        <FontAwesomeIcon nameClass="far fa-edit" />
+        <FontAwesomeIcon nameClass="far fa-images" />
+        <FontAwesomeIcon nameClass="fas fa-video" />
+        <FontAwesomeIcon nameClass="fas fa-map-marked-alt" />
+        <button>Опубликовать</button>
+      </form>
+    </AddPost>
+  )
+};
+
+
+const MyPostComponent = (props) => {
+  return (
+    <MyPost>
+      <PostHeader>
+        <Avatar>
+          <img src={props.userAvatarUrl} alt="user-avatar" />
+        </Avatar>
+        <UserName>
+          <h5>{ props.authorName }</h5>
+          <span>{props.postDate}</span>
+        </UserName>
+      </PostHeader>
+      <PostBody>
+        <p>{ props.userTextPost}</p>
+      </PostBody>
+    </MyPost>
+  )
+};
+
+
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
-const AllPosts = styled.div`
+const MyPost = styled.div`
   display: flex;
   flex-direction: column;
-  & h2 {
-    color: #000;
-    text-align: center;
-    margin-top: 40px;
-  }
-  & p {
-    color: #111;
-    font-size: 24px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: 100%;
+  width: 100%;
+  padding: 15px 20px;
+  background: rgb(250,250,250);
+  border: 1px solid rgba(204, 204, 204, 0.2);
+  border-radius: 6px;
+  margin-top: 20px;
+  transition: all 250ms ease-in 0ms;
+  &:hover {
+    box-shadow: 0 0 1px 0 rgba(0,0,0,0.1),
+                0 0 5px 0 rgba(0,0,0,0.1),
+                0 0 8px 0 rgba(0,0,0,0.1);
   }
 `;
+const PostHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content; center;
+  align-items: center;
+  height: 60px;
+  width: 100%;
+`;
+const UserName = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  & h5 {
+    color: rgb(42 88 133);
+    font-family: 'Montserrat', sans-serif;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 16px;
+    text-align: left;
+    margin-bottom: 5px;
+  }
+  & span {
+    line-height: 14px;
+    font-size: 13px;
+    font-weight: 400;
+    color: rgb(98 109 122);
+  }
+`;
+const PostBody = styled.div`
+  display: flex;
+  justify-content; center;
+  align-items: center;
+  margin-top: 15px;
+  & p {
+    font-size: 13px;
+    color: #000;
+    font-weight: 400;
+    line-height: 160%;
+    overflow: hidden;
+    word-wrap: break-word;
+    text-align: left;
+  }
+`;
+
 
 
 export default MyPosts;
